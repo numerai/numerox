de831768952b9676e25d433044f9a34a381e9390
Keith Goodman <kwgoodman@gmail.com>
2017-11-04 12:13:41 -0700

    refactor load_report

--- numerox/report.py
+++ numerox/report.py
@@ -11,32 +11,33 @@ class Report(object):
     def __init__(self, df=None):
         self.df = df
 
+    @staticmethod
+    def from_data_predictions(data, prediction_dict):
+        dfs = []
+        for model in prediction_dict:
+            df = prediction_dict[model].df
+            df.rename(columns={'yhat': model}, inplace=True)
+            dfs.append(df)
+        df = pd.concat(dfs, axis=1, verify_integrity=True, copy=False)
+        ery = data.df[['era', 'region', 'y']]
+        df = pd.merge(ery, df, left_index=True, right_index=True, how='right')
+        return Report(df)
+
     def performance(self):
         pass
 
 
-def load_report(data, prediction_dir, extension='.pred'):
+def load_report(data, prediction_dir, extension='pred'):
     "Load Prediction objects (hdf) in `prediction_dir`; return Report object"
-
-    # load prediction objects
     original_dir = os.getcwd()
     os.chdir(prediction_dir)
-    dfs = []
+    predictions = {}
     try:
         for filename in glob.glob("*{}".format(extension)):
             prediction = load_prediction(filename)
-            df = prediction.df
-            model = filename[:-len(extension)]
-            df.rename(columns={'yhat': model}, inplace=True)
-            dfs.append(df)
+            model = filename[:-len(extension) - 1]
+            predictions[model] = prediction
     finally:
         os.chdir(original_dir)
-
-    # concatenate predictions
-    df = pd.concat(dfs, axis=1, verify_integrity=True, copy=False)
-
-    # add in era, region, and y info
-    ery = data.df[['era', 'region', 'y']]
-    df = pd.merge(ery, df, left_index=True, right_index=True, how='right')
-
-    return Report(df)
+    report = Report.from_data_predictions(data, predictions)
+    return report
