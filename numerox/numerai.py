ff16eaec160ce0690bc7cae756732c58e191a2bc
Keith Goodman <kwgoodman@gmail.com>
2017-11-09 09:21:29 -0800

    add Submission class; refactor upload_submission

--- numerox/numerai.py
+++ numerox/numerai.py
@@ -20,7 +20,7 @@ def download_dataset(saved_filename):
             fd.write(chunk)
 
 
-def upload_submission(full_filename, public_id, secret_key, verbose=True):
+def upload_submission(filename, public_id, secret_key, verbose=True):
     """
     Upload tournament submission (csv file) to Numerai.
 
@@ -28,60 +28,15 @@ def upload_submission(full_filename, public_id, secret_key, verbose=True):
     upload_submission and read_submission_info. If verbose is False then only
     upload_submission is needed.
     """
-    t0 = time.time()
-    api = Numerai(public_id, secret_key)
-    if not api.has_token():
-        raise ValueError("Must supply public_id, secret_key")
-    filename = os.path.basename(full_filename)
-    auth_query = \
-        '''
-        query($filename: String!) {
-            submission_upload_auth(filename: $filename) {
-                filename
-                url
-            }
-        }
-        '''
-    submission_resp = api.call(auth_query, {'filename': filename})
-    submission_auth = submission_resp['data']['submission_upload_auth']
-    file_object = open(full_filename, 'rb').read()
-    requests.put(submission_auth['url'], data=file_object)
-    create_query = \
-        '''
-        mutation($filename: String!) {
-            create_submission(filename: $filename) {
-                id
-            }
-        }
-        '''
-    create = api.call(create_query, {'filename': submission_auth['filename']})
-    submission_id = create['data']['create_submission']['id']
-
-    # diplay status until complete
-    if verbose:
-        seen = []
-        print("            minutes")
-        fmt = "{:>10.6f}  {:<.4f}  {:<}"
-        while True:
-            status = submission_status(submission_id, public_id, secret_key)
-            t = time.time()
-            for key, value in status.items():
-                if value is not None and key not in seen:
-                    seen.append(key)
-                    minutes = (t - t0) / 60
-                    print(fmt.format(value, minutes, key))
-            if len(status) == len(seen):
-                break
-            if t - t0 > 300:
-                time.sleep(20)
-            else:
-                time.sleep(5)
-
-    return submission_id
+    sub = Submission(public_id, secret_key)
+    sub.upload(filename)
+    sub.status_block(verbose)
 
 
 def submission_status(submission_id, public_id, secret_key):
     "display submission status"
+    if submission_id is None:
+        raise ValueError('`submission_id` cannot be None')
     api = Numerai(public_id, secret_key)
     if not api.has_token():
         raise ValueError("Must supply public_id, secret_key")
@@ -113,6 +68,87 @@ def submission_status(submission_id, public_id, secret_key):
             value = value['value']
         status[key] = value
     return status
+
+
+class Submission(object):
+
+    def __init__(self, public_id, secret_key):
+        self.api = Numerai(public_id, secret_key)
+        if not self.api.has_token():
+            raise ValueError("Must supply public_id, secret_key")
+        self.t0 = None
+        self.submission_id = None
+        self.status = None
+
+    def upload(self, filename):
+        self.t0 = time.time()
+        auth_query = \
+            '''
+            query($filename: String!) {
+                submission_upload_auth(filename: $filename) {
+                    filename
+                    url
+                }
+            }
+            '''
+        variable = {'filename': os.path.basename(filename)}
+        submission_resp = self.api.call(auth_query, variable)
+        submission_auth = submission_resp['data']['submission_upload_auth']
+        file_object = open(filename, 'rb').read()
+        requests.put(submission_auth['url'], data=file_object)
+        create_query = \
+            '''
+            mutation($filename: String!) {
+                create_submission(filename: $filename) {
+                    id
+                }
+            }
+            '''
+        variables = {'filename': submission_auth['filename']}
+        create = self.api.call(create_query, variables)
+        self.submission_id = create['data']['create_submission']['id']
+        return self.submission_id
+
+    def status(self):
+        "status dictionary"
+        self.status = submission_status(self.submission_id, *self.api.token)
+        return self.status
+
+    def status_block(self, verbose=True):
+        "block until until status completes; then return status_dict"
+        seen = []
+        fmt = "{:>10.6f}  {:<.4f}  {:<}"
+        while True:
+            status = submission_status(self.submission_id, *self.api.token)
+            t = time.time()
+            for key, value in status.items():
+                if value is not None and key not in seen:
+                    seen.append(key)
+                    minutes = (t - self.t0) / 60
+                    if verbose:
+                        print(fmt.format(value, minutes, key))
+            if len(status) == len(seen):
+                break
+            seconds = min(5 + int((t - self.t0) / 100.0), 30)
+            time.sleep(seconds)
+        self.status = status
+        return self.status
+
+    @property
+    def upload_completed(self):
+        if self.submission_id is None:
+            return False
+        return True
+
+    @property
+    def status_completed(self):
+        return self._is_status_complete(self.status)
+
+    def _is_status_complete(self, status):
+        n_none = status.values().count(None)
+        if n_none > 0:
+            return False
+        return True
 
 
 # ---------------------------------------------------------------------------
