3e724bfd0a6b6b7b3df04bf899bd01c87a0cd325
Keith Goodman <kwgoodman@gmail.com>
2017-11-14 14:25:42 -0800

    set release date to today!

--- release.rst
+++ release.rst
@@ -9,6 +9,6 @@ see the commit log at http://github.com/kwgoodman/numerox
 numerox 0.0.1
 =============
 
-*Release date: Not yet released; in development*
+*Release date: 2017-11-14*
 
 - Preview release of numerox
--- setup.py
+++ setup.py
@@ -27,10 +27,11 @@ def get_version_str():
 
 
 CLASSIFIERS = [
-    "Development Status :: 3 - Alpha",
+    "Development Status :: 4 - Beta",
     "Environment :: Console",
     "Intended Audience :: Science/Research",
-    "License :: OSI Approved :: BSD License",
+    "License :: OSI Approved :: GNU General Public License v3 "
+    "or later (GPLv3+)",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2",
