38e8f932b3e7d28c2b384b0ff3f3d050d5907c7e
Keith Goodman <kwgoodman@gmail.com>
2017-12-09 12:17:49 -0800

    add ability to load new Numerai datasets

--- numerox/data.py
+++ numerox/data.py
@@ -11,7 +11,7 @@ HDF_DATA_KEY = 'numerox_data'
 ERA_INT_TO_STR = {}
 ERA_STR_TO_INT = {}
 ERA_STR_TO_FLOAT = {}
-for i in range(120):
+for i in range(150):
     name = 'era' + str(i)
     ERA_INT_TO_STR[i] = name
     ERA_STR_TO_INT[name] = i
