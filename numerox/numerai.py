e0f468a5eb53807ccbf60e613c844dd0b3100f18
Keith Goodman <kwgoodman@gmail.com>
2018-02-09 09:38:48 -0800

    remove $0 rows from 1099 report

--- numerox/numerai.py
+++ numerox/numerai.py
@@ -238,6 +238,7 @@ def ten99(user, year=2017):
     total = df['usd_main'].values + df['usd_stake'].values
     total = total + df['nmr_main'].values * df['nmr_usd'].values
     df['total'] = total
+    df = df[df['total'] != 0]
     date = tournament_resolution_date()
     date = date.loc[df.index]
     df.insert(0, 'date', date)
