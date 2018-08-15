047c5f1b3ac04cf0b19bd0bac909e6301dff4d17
Keith Goodman <kwgoodman@gmail.com>
2018-08-15 08:20:15 -0700

    fix broken import

--- numerox/examples/__init__.py
+++ numerox/examples/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 
 from numerox.examples.run_all_examples import run_all_examples
-from numerox.examples.first_tournament import first_tournament
+from numerox.examples.first_tournament import numerox_example
 from numerox.examples.backtest_example import backtest_example
 from numerox.examples.concordance_example import concordance_example
 from numerox.examples.compare_models import compare_models
