8adaec0bc52e11da16c99dcac610be674d9a955b
CSP <16367956+CSP197@users.noreply.github.com>
2019-07-15 19:31:57 -0700

    Minor cosmetic changes

--- numerox/data.py
+++ numerox/data.py
@@ -687,10 +687,10 @@ class Y(object):
             if index in nx.tournament_all(as_str=True, active_only=True):
                 return self2.df[index].values
             else:
-                raise IndexError('string index not recognized')
+                raise IndexError('string index `{}` not recognized'.format(index))
         elif nx.isint(index):
             if index < 1 or index > n:
-                txt = 'tournament number must be between 1 and {}'
+                txt = 'tournament number `{}` must be between 1 and {}'.format(index, n)
                 raise IndexError(txt.format(n))
             return self2.df[nx.tournament_str(index)].values
         elif isinstance(index, slice):
